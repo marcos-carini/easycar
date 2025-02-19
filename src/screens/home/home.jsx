97077bb994ba1d0587fd35ff00e4af27b1b68585
Marcos Vinicius <marcos.carini22@gmail.com>
2025-02-19 03:44:23 -0300

    chore: add react-navigation dependencies

--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -1,19 +1,33 @@
-import { Image, ImageBackground, Text, TouchableOpacity } from "react-native";
+import {
+  Alert,
+  Image,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import icons from "../../constants/icons.js";
 import { styles } from "./home.style.js";
 
 function Home() {
+  function OpenPassenger() {
+    Alert.alert("Abrir tela 1");
+  }
+
+  function OpenRide() {
+    Alert.alert("Abrir tela 2");
+  }
+
   return (
     <ImageBackground source={icons.bg} resizeMode="cover" style={styles.bg}>
       <Image source={icons.logo} style={styles.logo} />
 
-      <TouchableOpacity style={styles.btn}>
+      <TouchableOpacity style={styles.btn} onPress={OpenPassenger}>
         <Image source={icons.passenger} style={styles.img} />
         <Text style={styles.title}>Passageiro</Text>
         <Text style={styles.text}>Encontre uma carona para você</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.btn}>
+      <TouchableOpacity style={styles.btn} onPress={OpenRide}>
         <Image source={icons.driver} style={styles.img} />
         <Text style={styles.title}>Motorista</Text>
         <Text style={styles.text}>Ofereça carona em seu carro</Text>
