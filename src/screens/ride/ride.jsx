33d7ffca1f88580ce19eab60d87c3a7d6ef6b079
Marcos Vinicius <marcos.carini22@gmail.com>
2025-02-26 20:26:39 -0300

    feat: integrate API on ride screen to retrieve required data

--- src/screens/ride/ride.jsx
+++ src/screens/ride/ride.jsx
@@ -1,8 +1,8 @@
 import { FlatList, View, Text, TouchableOpacity, Image } from "react-native";
 import { styles } from "./ride.style.js";
-import { json_rides } from "../../constants/dados.js";
 import icons from "../../constants/icons.js";
 import { useEffect, useState } from "react";
+import { api, HandleError } from "../../constants/api.js";
 
 function Ride(props) {
   const userId = 2;
@@ -13,7 +13,16 @@ function Ride(props) {
   }
 
   async function RequestRides() {
-    setRides(json_rides);
+    try {
+      const response = await api.get("/rides/drivers/" + userId);
+
+      if (response.data) {
+        console.log(response.data);
+        setRides(response.data);
+      }
+    } catch (error) {
+      HandleError(error);
+    }
   }
 
   useEffect(() => {
@@ -33,7 +42,10 @@ function Ride(props) {
               onPress={() => ClickRide(item.ride_id)}
             >
               <View style={styles.containerName}>
-                <Image source={icons.car} style={styles.car} />
+                {item.driver_user_id == userId && (
+                  <Image source={icons.car} style={styles.car} />
+                )}
+
                 <Text style={styles.name}>{item.passenger_name}</Text>
               </View>
               <Text style={styles.address}>Origem: {item.pickup_address}</Text>
